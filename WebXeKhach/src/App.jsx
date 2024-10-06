411c91e502c4a1de703cd48246d5e97f77720340
karuto1311 <hhh131120044@gmail.com>
2024-10-06 07:14:19 +0700

    Update code lai nhin cho gon 1 xi

--- WebXeKhach/src/App.jsx
+++ WebXeKhach/src/App.jsx
@@ -3,15 +3,14 @@ import './App.css'
 import Navbar from './Components/Navbar'
 import MainContent from './Components/MainContent'
 import Footer from './Components/Footer'
+import Home from './Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <Navbar />
-      <MainContent />
-      <Footer />
+      <Home />
   </div>
   )
 }
---
+++ WebXeKhach/src/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+import MainContent from './Components/MainContent'
+import Footer from './Components/Footer'
+
+function Home() {
+  return (
+    <div className="Home">
+        <Navbar />
+        <MainContent />
+        <Footer />
+    </div>  )
+}
+
+export default Home
