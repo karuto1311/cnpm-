2046efa389ee716f66114f967cc191bc779ae184
Trancongdanh2004 <danhtrancong956@gmail.com>
2024-10-10 14:58:37 +0700

    test tinh nang comitt

--- WebXeKhach/src/Home.jsx
+++ WebXeKhach/src/Home.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
-import Navbar from './Components/Navbar'
-import MainContent from './Components/MainContent';
-import Footer from './Components/Footer'
-
+import React from "react";
+import Navbar from "./Components/Navbar";
+import MainContent from "./Components/MainContent";
+import Footer from "./Components/Footer";
 
 function Home() {
-
   return (
     <div className="Home">
-        <Navbar />
-        <MainContent />
-        <Footer />
-    </div>  )
+      <Navbar />
+      <MainContent />
+      <Footer />
+    </div>
+  );
 }
 
-export default Home
+export default Home;
