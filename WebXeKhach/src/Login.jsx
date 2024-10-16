994efbc8c12cf3ebcb6c232aa50e07dd0033a9ca
BaoNguyen20040407 <baonguyenhuflit@gmail.com>
2024-10-16 20:18:16 +0700

    Update Đăng nhập (FE)

--- WebXeKhach/src/Login.jsx
+++ WebXeKhach/src/Login.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
-import Login from "./Components/Login"; // Đảm bảo đường dẫn đúng
+import Login from "./Components/Login"; 
 
 function LoginPage() {
     return (
