0d1ff90fbe00bc70d6e00e0dcb8ad0f5209af590
BaoNguyen20040407 <baonguyenhuflit@gmail.com>
2024-10-16 21:00:41 +0700

    Chỉnh sửa navbar

--- WebXeKhach/src/Components/Navbar.jsx
+++ WebXeKhach/src/Components/Navbar.jsx
@@ -1,34 +1,39 @@
 import React from 'react';
-import { Link } from 'react-router-dom';  // Lưu ý import Link từ 'react-router-dom'
+import { Link } from 'react-router-dom';
 import '../assets/Css/Navbar.css';
 import logo from '../assets/logo.png';
 
 function Navbar() {
   return (
     <nav className="navbar">
-      <ul className="navbar-left">
-        <li><Link to="/">Trang chủ</Link></li>
-        <li><Link to="/schedule">Lịch trình</Link></li>
-        <li><Link to="/ticket">Tra cứu vé</Link></li>
-        <li><Link to="/news">Tin tức</Link></li>
-        <li><Link to="/contact">Liên hệ</Link></li>
-        <li><a href="/about">Về chúng tôi</a></li>
+      <div className="navbar-top">
+        <div className="left-section">
+          <div className="footer-note">@NamHai2004 - 20NamDongHanh</div>
+        </div>
+        <div className="logo-container">
+          <img src={logo} alt="Xe Dai Nam" className="logo" />
+        </div>
+        <div className="loginbtn">
+          <li>
+            <Link to="/login">
+              <button>Đăng Nhập</button>
+            </Link>
+          </li>
+          <li>
+            <Link to="/">
+              <button>Đăng Ký</button>
+            </Link>
+          </li>
+        </div>
+      </div>
+      <ul className="navbar-bottom">
+        <li><Link to="/">TRANG CHỦ</Link></li>
+        <li><Link to="/schedule">LỊCH TRÌNH</Link></li>
+        <li><Link to="/ticket">TRA CỨU VÉ</Link></li>
+        <li><Link to="/news">TIN TỨC</Link></li>
+        <li><Link to="/contact">LIÊN HỆ</Link></li>
+        <li><a href="/about">VỀ CHÚNG TÔI</a></li>
       </ul>
-      <div className="logo-container">
-        <img src={logo} alt="Xe Dai Nam" className="logo" />
-      </div>
-      <div className="loginbtn">
-      <li>
-          <Link to="/login">
-            <button>Đăng Nhập</button>
-          </Link>
-      </li>
-      <li>
-          <Link to="/">
-            <button>Đăng Ký</button>
-          </Link>
-      </li>
-      </div>
     </nav>
   );
 }
