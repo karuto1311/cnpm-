6edad034f9fdff11e0e392c23c432c99a43326dd
karuto1311 <hhh131120044@gmail.com>
2024-10-24 10:00:14 +0700

    tao phan temple cho backend

---
+++ WebXeKhach/src/backend/be.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import bacend from '../backendcomponents/backend'
+
+function be() {
+  return (
+    <div>be</div>
+  )
+}
+
+export default be
---
+++ WebXeKhach/src/backendcomponents/backend.js
@@ -0,0 +1,4 @@
+// day la phan cua
+
+
+import '../assets/backendCss/'
